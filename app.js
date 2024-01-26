bda62dc659c2517addd24650f4ba1f79c3bd4cf8
Ophélie <roux.ophelie.or@gmail.com>
2024-01-26 12:03:15 +0100

    MM

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
